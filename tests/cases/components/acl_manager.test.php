9e8b196690370c1ce7a511491a03e1e0a7f4aeed
Fumiya Nakamura <nakamurafumiya003@gmail.com>
2013-03-03 19:04:20 -0800

    Added AclReflectorComponent test

---
+++ tests/cases/components/acl_manager.test.php
@@ -0,0 +1,51 @@
+<?php
+/* AclManager Test cases generated on: 2013-03-04 05:18:18 : 1362341898*/
+App::import('Component', 'Acl.AclManager');
+App::import('Model', 'Acl.AdminUser');
+App::import('Model', 'Acl.AdminGroup');
+
+class FakeAclManagerController {}
+
+class AclManagerComponentTestCase extends CakeTestCase {
+	function startTest() {
+		$this->AclManager =& new AclManagerComponent();
+		$controller = new FakeAclManagerController();
+		//$controller->AclReflector = new FakeAclManagerTest();
+		$this->AclManager->initialize(&$controller);
+	}
+
+	function endTest() {
+		unset($this->AclManager);
+		ClassRegistry::flush();
+	}
+
+	function testCheckUserModelActsAsAclRequester() {
+		$result = $this->AclManager->checkUserModelActsAsAclRequester('Aco');
+		$this->assertFalse($result);
+		$result = $this->AclManager->checkUserModelActsAsAclRequester('AdminUser');
+		$this->assertTrue($result);
+		$result = $this->AclManager->checkUserModelActsAsAclRequester('AdminGroup');
+		$this->assertTrue($result);
+	}
+
+	function testGetStoredControllersHashes() {
+
+	}
+
+	function testGetCurrentControllersHashes() {
+
+	}
+
+	function testGetMissingAcos() {
+
+	}
+
+	function testCreateAcos() {
+
+	}
+
+	function testSavePermission() {
+
+	}
+
+}
